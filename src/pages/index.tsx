a5ab5d8bbe424e5a2c6b4af81bac2fe54e84484d
man.nh <man.nh@metatechnologylab.io>
2024-08-27 17:17:08 +0700

    [feat] update i18n and storage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,28 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useRouter } from 'next/router';
 
 import { RootState } from '@/redux/store';
 import { decrement, increment } from '@/redux/slices/homeSlice';
+import storage from '@/utils/storage';
 
 import Button from '@/components/button/Button';
 
 const HomePage: React.FC = () => {
   const value = useSelector((state: RootState) => state.home.value);
   const dispatch = useDispatch();
-  const { t } = useTranslation('common');
+  const { t, i18n } = useTranslation('common');
+  const router = useRouter();
+  const { pathname, asPath, query } = router;
+
+  const changeLanguage = (locale: string) => {
+    router.replace({ pathname, query }, asPath, {
+      locale: locale,
+    });
+    i18n.changeLanguage(locale);
+    storage().set('locale', locale)
+  };
 
   return (
     <div>
@@ -19,6 +31,9 @@ const HomePage: React.FC = () => {
       <p>{t('welcome')}</p>
 
       <h1>{value}</h1>
+
+      <Button onClick={() => changeLanguage('en')}>English</Button>
+      <Button onClick={() => changeLanguage('vi')}>Tiếng Việt</Button>
       <Button onClick={() => dispatch(increment())}>Increment</Button>
       <Button onClick={() => dispatch(decrement())}>Decrement</Button>
     </div>
@@ -30,6 +45,7 @@ export default HomePage;
 export async function getServerSideProps({ locale }: any) {
   return {
     props: {
+      initialLocale: locale,
       ...(await serverSideTranslations(locale, ['common'])),
     },
   };
